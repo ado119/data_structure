713470fb86ad972056bde40044b04c4cf1e9e075
ado119 <43161124+ado119@users.noreply.github.com>
2019-03-20 11:45:38 +0900

    과제 수정
    100개를 순환으로 돌려도 시간체크가 0이 나온다...

--- data_source/circulation/subject_0/subject.c
+++ data_source/circulation/subject_0/subject.c
@@ -1,27 +1,31 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
-int arr[10];
+int arr[100];
 void append(int num, int n)
 {
 	arr[n - 1] = num;
 }
 int find_max_num(int arr[], int n)
 {
-	if (n == 1)	
-		return max(arr[n], arr[n - 1]);
-	return max(arr[n], find_max_num(arr, n-1));
+	if (n == 1)
+		return max_num(arr[n], arr[n - 1]);
+	return max_num(arr[n], find_max_num(arr, n - 1));
 }
-int max(int x, int y)
+int max_num(int x, int y)
 {
 	if (x > y)
 		return x;
-	return y;	
+	return y;
 }
 int find_min_num(int arr[], int n)
 {
-	return 0;
+	if (n == 1)
+		return min_num(arr[n], arr[n - 1]);
+	return min_num(arr[n], find_min_num(arr, n - 1));
 }
-int min(int x, int y)
+int min_num(int x, int y)
 {
 	if (x < y)
 		return x;
@@ -31,7 +35,9 @@ int min(int x, int y)
 int main()
 {
 	int check_find, max_num, min_num;
-	for (int num = 1; num < 11; num++)
+	clock_t start, stop;
+	double duration;
+	for (int num = 1; num < 101; num++)
 	{
 		int append_num;
 		printf("배열에 추가할 %d번째 숫자를 적으세요. ", num);
@@ -43,7 +49,7 @@ int main()
 	printf("배열 전체 출력 {");
 	for (int k = 0; k < len; k++)
 	{
-		printf("%d ",arr[k]);
+		printf("%d ", arr[k]);
 	}
 	printf("}\n");
 	printf("2단계 완료\n");
@@ -51,17 +57,25 @@ int main()
 		printf("최대값(0) or 최소값(1) or 반복그만(2): ");
 		scanf("%d", &check_find);
 		if (check_find == 0) {
-			max_num = find_max_num(arr, 9);
+			start = clock();
+			max_num = find_max_num(arr, 99);
+			stop = clock();
+			duration = (double)(stop - start) / CLOCKS_PER_SEC;
 			printf("최대값은 %d\n", max_num);
+			printf("수행시간은 %f초입니다.\n", duration);
 		}
 		else if (check_find == 1) {
-			min_num = find_min_num(arr, 9);
-			printf("최대값은 %d\n", min_num);
+			start = clock();
+			min_num = find_min_num(arr, 99);
+			stop = clock();
+			duration = (double)(stop - start) / CLOCKS_PER_SEC;
+			printf("최소값은 %d\n", min_num);
+			printf("수행시간은 %f초입니다.\n", duration);
 		}
 		else {
 			break;
 		}
 	}
-	
+
 	return 0;
 }
